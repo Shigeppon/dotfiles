877200d7233d7e7caf6ccfc073c65b494dac09b0
Shigeo Sakamot <shigeo.sakamoto@gmail.com>
2012-12-02 13:05:12 +0900

    dotfilesを作成開始

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+CVSROOT=~/cvsroot
+export CVSROOT
+SVN_REPO=svn+ssh://pocchi.local/Users/shigeo/svnroot
+export SVN_REPO
+SVN_EDITOR="VIM"
+export SVN_EDITOR
+export PATH=/opt/local/bin:/opt/local/sbin/:$PATH
+export MANPATH=/opt/local/man:$MANPATH
+
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# backup用ディレクトリの作成
+if [ -e $HOME/backup ]; then
+    echo "backup is exist"
+else
+    mkdir $HOME/backup
+fi
+
+#シンボリックリンクの作成
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/.bashrc_profile $HOME/.bashrc_profile
+ln -sf $HOME/dotfiles/.vim $HOME/.vim
+ln -sf $HOME/dotfiles/.profile $HOME/.profile
