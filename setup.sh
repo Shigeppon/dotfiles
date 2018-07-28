d9b2f0bcaa614bbd896e5340fdba72ef113e00b5
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2018-07-28 11:37:03 +0900

    setup.shの実行パスを変更

--- setup.sh
+++ setup.sh
@@ -7,18 +7,20 @@ else
     mkdir $HOME/backup
 fi
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 #シンボリックリンクの作成
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.zcompdump $HOME/.zcompdump
-ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/dotfiles/.vim $HOME/.vim
-ln -sf $HOME/dotfiles/.profile $HOME/.profile
-ln -sf $HOME/dotfiles/.rspec $HOME/.rspec
+ln -sf $SCRIPT_DIR/.vimrc $HOME/.vimrc
+ln -sf $SCRIPT_DIR/.bashrc $HOME/.bashrc
+ln -sf $SCRIPT_DIR/.bash_profile $HOME/.bash_profile
+ln -sf $SCRIPT_DIR/.zshrc $HOME/.zshrc
+ln -sf $SCRIPT_DIR/.zcompdump $HOME/.zcompdump
+ln -sf $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
+ln -sf $SCRIPT_DIR/.vim $HOME/.vim
+ln -sf $SCRIPT_DIR/.profile $HOME/.profile
+ln -sf $SCRIPT_DIR/.rspec $HOME/.rspec
 #bashのカラー化はbsdではcolorlsで行うのでdir_colorsは使わい(gnuls用なので)
 #ln -sf $HOME/dotfiles/.dir_colors $HOME/.dir_colors
-ln -sf $HOME/dotfiles/.gitignore $HOME/.gitignore
-ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -sf $HOME/dotfiles/.screenrc $HOME/.screenrc
+ln -sf $SCRIPT_DIR/.gitignore $HOME/.gitignore
+ln -sf $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $SCRIPT_DIR/.screenrc $HOME/.screenrc
